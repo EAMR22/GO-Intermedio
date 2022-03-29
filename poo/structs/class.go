a4382814d79bd699881887c55f6af8cf87271a86
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-03-28 22:42:48 -0600

    metodos y funciones

--- poo/structs/class.go
+++ poo/structs/class.go
@@ -2,15 +2,40 @@ package main
 
 import "fmt"
 
-type Employe struct { // struct es equivalente a una clase en otros lenguajes de programacion.
+// Estructura de una clase en go:
+
+type Employe struct {
 	id   int
 	name string
 }
 
+// Creacion de metodos en go:
+
+func (e *Employe) SetId(id int) { // El struct Employe va a poseer un metodo llamado SetId.
+	e.id = id
+}
+
+func (e *Employe) SetName(name string) {
+	e.name = name
+}
+
+func (e *Employe) GetId() int {
+	return e.id
+}
+
+func (e *Employe) GetName() string {
+	return e.name
+}
+
 func main() {
-	e := Employe{} // Aqui instanciamos el objeto.
-	fmt.Printf("%v", e)
+	e := Employe{}
+	// fmt.Printf("%v", e)
 	e.id = 1
 	e.name = "name"
-	fmt.Printf("%v", e)
+	// fmt.Printf("%v", e)
+	e.SetId(5)
+	e.SetName("goolang")
+	// fmt.Printf("%v", e)
+	fmt.Println(e.GetId())
+	fmt.Println(e.GetName())
 }

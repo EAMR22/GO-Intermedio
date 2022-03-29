89b53354bfd6629cc77c8c25aebe8223d616624d
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-03-29 11:04:13 -0600

    Interfaces

--- poo/inheritance/class.go
+++ poo/inheritance/class.go
@@ -11,11 +11,37 @@ type Employee struct {
 	id int
 }
 
-// Es una composicion sobre la herencia:
-
 type FullTimeEmployee struct {
 	Person
 	Employee
+	endDte string
+}
+
+// Algunas funciones pueden funcionar como metodos.
+// Go lo hace de manera implicita a diferencia de otros lenguajes.
+func (ftEmployee FullTimeEmployee) getMessage() string {
+	return "Full Time Employee"
+}
+
+type TemporatyEmployee struct {
+	Person
+	Employee
+	taxRate int
+}
+
+// Se implementa la interfaz.
+func (tEmployee TemporatyEmployee) getMessage() string {
+	return "Temporaty Employee"
+}
+
+// Es una interface:
+type PrintInfo interface {
+	getMessage() string
+}
+
+// Se crea el metodo de la interfaz
+func getMessage(p PrintInfo) {
+	fmt.Println(p.getMessage())
 }
 
 func main() {
@@ -24,4 +50,7 @@ func main() {
 	ftEmployee.age = 2
 	ftEmployee.id = 5
 	fmt.Printf("%v\n", ftEmployee)
+	tEmployee := TemporatyEmployee{}
+	getMessage(tEmployee)
+	getMessage(ftEmployee)
 }

8f0b35ef8d1e97e8957a0c9df4f624ef017744de
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-03-31 22:09:53 -0600

    Profiling

--- testing/main_test.go
+++ testing/main_test.go
@@ -45,3 +45,21 @@ func TestMax(t *testing.T) {
 		}
 	}
 }
+
+func TestFib(t *testing.T) {
+	tables := []struct {
+		a int
+		n int
+	}{
+		{1, 1},
+		{8, 21},
+		{50, 12586269025},
+	}
+
+	for _, item := range tables {
+		fib := Fibonacci(item.a)
+		if fib != item.n {
+			t.Errorf("Fibonacci was incorrect, was %d expected %d", fib, item.n)
+		}
+	}
+}

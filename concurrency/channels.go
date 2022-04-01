9a43b16d0fa5130af074ed10390c9647c821b6e3
EAMR22 <elvis1042.rodriguez@gmail.com>
2022-04-01 11:26:49 -0600

    Unbuffered channels y buffered channels

---
+++ concurrency/channels.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int, 3) // Es un canal con buffer.
+
+	c <- 1 //No bloqueara el programa, porque tiene una capacidad para 3 elementos.
+	c <- 2
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+}
